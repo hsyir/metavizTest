4e379adb01a0f0803552da01a04d2639a8dc18af
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2023-01-16 10:22:35 +0330

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends("layouts.app")
+@section("content")
+<div class="container bg-white p-5">
+    <div class="row">
+        <div class="col-md-12">
+            <button class="btn btn-primary">Call</button>
+        </div>
+    </div>
+</div>
+<script>
+    
+</script>
+@endsection
